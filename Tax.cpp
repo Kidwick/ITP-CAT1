03bfd8832f42923563ad3143653430f7c7d65091
Kidwick <115428041+Kidwick@users.noreply.github.com>
2022-11-03 19:43:14 +0300

    Add files via upload

---
+++ Tax.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+    //Variable declarartion
+    int PAYE,grosssalary,netsalary,taxpayable;
+
+    //Variable initialisation
+    grosssalary=10,000;
+    const int PAYE=25% ;
+
+    //logic calculation
+
+    cout<<"taxpayable=25% * grosssalary";
+    cout<<"netsalary=grosssalary-taxpayable";
+
+    return 0;
+
+}
---
+++ Temperature.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+    //Variable declaration
+    int Fahrenheit,Celsius,Kelvin;
+
+    //Variable initailisation
+    cout<<"Enter Fahrenheit recording:";
+    cin>>Fahrenheit;
+
+    //logic calculation
+
+    cout<<"Celsius=5.0/9.0*(Fahrenheit-32.0);"
+
+    return 0;
+}
